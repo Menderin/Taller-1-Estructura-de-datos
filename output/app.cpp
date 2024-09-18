f938689a6b4ce5992280ec5d1b832d3e001b7c7a
Kevin <167124972+Liespren@users.noreply.github.com>
2024-09-18 15:56:54 -0300

    archivos.txt
    archivos:
    usuarios.txt
    textos.txt

--- output/app.cpp
+++ output/app.cpp
@@ -4,8 +4,6 @@
 #include "Revista.h"
 #include "Usuario.h"
 #include "Biblioteca.h"
-using namespace std;
-
 
 using namespace std;
 
@@ -13,7 +11,11 @@ using namespace std;
 int main(){
 
     //Biblioteca biblioteca();
-    Libro a("El nombre del viento", "Patrick Rothfuss", "DAW Books", "978-0-7564-0471-2", "1ra edición", "2007");
+    Libro a("El nombre del viento","978-0-7564-0471-2","Patrick Rothfuss","Y","2007","'El nombre del viento' sigue la historia de Kvothe, un joven prodigio que narra su vida desde su infancia en una troupe de artistas itinerantes hasta su búsqueda de conocimiento y venganza en la universidad mágica.");
     a.mostrarInformacion();
+    cout<< "---" << endl;
+
+    Libro b ("El camino de los reyes","978-84-08-13008-0","Brandon Sanderson","Y","2010","'El camino de los reyes' es el primer libro de la serie 'El Archivo de las Tormentas', y sigue las historias entrelazadas de varios personajes en un mundo de magia y guerra, explorando sus destinos y las fuerzas que los moldean.");
+    b.mostrarInformacion();
     return 0; 
 }
